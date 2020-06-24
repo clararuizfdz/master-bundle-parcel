95d509ec3b4ba21dc56c8f0edb3d1ea01f412900
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-06-24 08:54:19 +0200

    Add postcss to load styles in react components

--- src/header.tsx
+++ src/header.tsx
@@ -3,6 +3,8 @@ const logo = require("./content/source.gif");
 const classes = require("./headerStyles.scss");
 
 export const Header: React.FC = () => {
+  console.log(classes);
+  console.log(classes.header);
   return (
       <div className={classes.header}>
         <img src={logo} className={classes.logo} />
