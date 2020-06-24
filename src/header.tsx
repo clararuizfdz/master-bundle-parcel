837883d9d45010a4f4eb753608f369142c331f5c
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-06-24 10:17:16 +0200

    Add environment variables

--- src/header.tsx
+++ src/header.tsx
@@ -3,8 +3,6 @@ const logo = require("./content/source.gif");
 const classes = require("./headerStyles.scss");
 
 export const Header: React.FC = () => {
-  console.log(classes);
-  console.log(classes.header);
   return (
       <div className={classes.header}>
         <img src={logo} className={classes.logo} />
