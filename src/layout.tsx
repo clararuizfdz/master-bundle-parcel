421ed0c239607a0a9dd2d96945f4ee60998b092d
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-06-24 10:19:10 +0200

    Rename layout styles

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import {Header} from "./header";
 import {Footer} from "./footer";
-const classes = require("./mystyles.scss");
-
+const classes = require("./layoutStyles.scss");
 
 export const Layout: React.FC = () => {
   return (
